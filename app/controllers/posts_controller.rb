2cfb6d16b8b77f95e650752085bab26a4913a2de
simaolemos <spcl_vnf@hotmail.com>
2018-03-01 16:38:11 +0000

    []

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to :back
+    else
+      flash[:notice] = "Something wrong happening... Try again later."
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def post_params
+      params.require(:post).permit(:body)
+  end
 end
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,8 @@ class ProfileController < ApplicationController
 
   def index
     user = User.find(current_user.id)
+
+    @post = Post.new
     @posts = user.posts
   end
 
