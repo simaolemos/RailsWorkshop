30103d9ceaf6820aebf40084583f522c0eeb512f
simaolemos <spcl_vnf@hotmail.com>
2018-03-05 10:40:21 +0000

    [add] Post new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
   def index
   end
 
+  def new
+    @post = Post.new
+  end
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
