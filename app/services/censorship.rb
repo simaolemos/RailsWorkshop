fd524cb3091adeff2361d1a1b1debcfcebba6589
simaolemos <spcl_vnf@hotmail.com>
2018-03-05 15:20:37 +0000

    [refactoring] Post spec

--- app/services/censorship.rb
+++ app/services/censorship.rb
@@ -2,8 +2,10 @@ module Censorship
   class << self
     def censorship
       before_save do
-        response = HTTParty.get(URI.encode("http://poopfilter.herokuapp.com/filter?text=#{self.body}"))
-        self.body = JSON.parse(response.body)["output"]
+        if body_changed?
+          response = HTTParty.get(URI.encode("http://poopfilter.herokuapp.com/filter?text=#{self.body}"))
+          self.body = JSON.parse(response.body)["output"]
+        end
       end
     end
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe Post, type: :model do
 
   context "Should support any test" do
 
+    let(:expected_body) { 'REDACTED U MotherREDACTEDer' }
+    let(:user) { { email: "test1@test.com", username: "test", password: '123321' } }
+    let(:body) { { body: 'Fuck U MotherFucker' } }
+
     before do
-      @User = User.create!(email: "test1@test.com", username: "test", password: '123321')
+      @User = User.create!(user)
     end
 
     it "Should insert a new post" do
@@ -15,10 +19,10 @@ RSpec.describe Post, type: :model do
 
 
     it "Should check if the post has are censured" do
-      post = @User.posts.new(body: "Fuck U MotherFucker", user_id: 1)
+      post = @User.posts.new(body)
       post.save!
 
-      expect(post.body).to eq("REDACTED U MotherREDACTEDer")
+      expect(post.body).to eq(expected_body)
     end
   end
 
